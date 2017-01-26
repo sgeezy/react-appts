ec7bb1b56ba6ae5a1e8d97e39cc31be6efe53b22
sgeezy <gmixlpixl@gmail.com>
2017-01-26 03:17:02 +0000

    Update vendor assets to get css and js to work

--- app/assets/javascripts/application.js
+++ app/assets/javascripts/application.js
@@ -1,8 +1,8 @@
 // This is a manifest file that'll be compiled into application.js, which will include all the files
 // listed below.
 //
-// Any JavaScript/Coffee file within this directory, lib/assets/javascripts, vendor/assets/javascripts,
-// or any plugin's vendor/assets/javascripts directory can be referenced here using a relative path.
+// Any JavaScript/Coffee file within this directory, lib/javascripts, vendor/javascripts,
+// or any plugin's vendor/javascripts directory can be referenced here using a relative path.
 //
 // It's not advisable to add code directly here, but if you do, it'll appear at the bottom of the
 // compiled file.
@@ -13,4 +13,10 @@
 //= require jquery
 //= require jquery_ujs
 //= require turbolinks
+//= require js/jquery.min.js
+//= require js/jquery.scrollex.min.js
+//= require js/jquery.scrolly.min.js
+//= require js/skel.min.js
+//= require js/util.js
+//= require js/main.js
 //= require_tree .
