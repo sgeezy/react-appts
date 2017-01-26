478e6db986d67a7f441134e6914efa51cdd6cf1e
sgeezy <gmixlpixl@gmail.com>
2017-01-26 18:53:40 +0000

    Update banner section, section 1 with new copy; Add screen grab and description with link to first portfolio entry for DDV

--- spec/views/pages/index.html.erb_spec.rb
+++ spec/views/pages/index.html.erb_spec.rb
@@ -1,5 +1,9 @@
 require 'rails_helper'
 
 RSpec.describe "pages/index.html.erb", type: :view do
-  pending "add some examples to (or delete) #{__FILE__}"
+  it "Expect links layout to work properly" do
+     render
+     
+     
+  end
 end
