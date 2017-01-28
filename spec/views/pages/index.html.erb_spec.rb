4d71c579d569d44a925abea043cefc23708a166d
sgeezy <gmixlpixl@gmail.com>
2017-01-28 01:50:39 +0000

    Revert "Update banner section, section 1 with new copy; Add screen grab and description with link to first portfolio entry for DDV"
    This reverts commit 478e6db986d67a7f441134e6914efa51cdd6cf1e.

--- spec/views/pages/index.html.erb_spec.rb
+++ spec/views/pages/index.html.erb_spec.rb
@@ -1,9 +1,5 @@
 require 'rails_helper'
 
 RSpec.describe "pages/index.html.erb", type: :view do
-  it "Expect links layout to work properly" do
-     render
-     
-     
-  end
+  pending "add some examples to (or delete) #{__FILE__}"
 end
