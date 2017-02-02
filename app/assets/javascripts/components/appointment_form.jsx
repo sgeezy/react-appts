407366edc7ee07d7f4bacdaff865100c8f7353b9
sgeezy <gmixlpixl@gmail.com>
2017-02-02 03:20:04 +0000

    Lessons 3.3 to 3.4 thru minute 4:50

--- app/assets/javascripts/components/appointment_form.jsx
+++ app/assets/javascripts/components/appointment_form.jsx
@@ -1,11 +1,27 @@
 var AppointmentForm = React.createClass({
+    handleChange: function(e) {
+        var name = e.target.name;
+        obj = {};
+        obj[name] = e.target.value;
+        console.log(obj);
+        console.log(this.props.onUserInput);
+        this.props.onUserInput(obj);
+    },
+    handleSubmit: function(e) {
+        e.preventDefault();
+        this.props.onFormSubmit();
+    },
+    
     render: function() {
         return(
             <div>
                 <h2>Make a New Appointment</h2>
-                <form>
-                    <input name='title' placeholder='Appointment Title' />
-                    <input name='appt_time' placeholder='Date and Time' />
+                <form onSubmit={this.handleSubmit}>
+                    <input name='title' placeholder='Appointment Title' 
+                    value={this.props.input_title}
+                    onChange={this.handleChange} />
+                    <input name='appt_time' placeholder='Date and Time' value={this.props.input_appt_time}
+                    onChange={this.handleChange} />
                     <input type='submit' value='Make Appointment' />
                 </form>
             </div>    
--- app/assets/javascripts/components/appointments.jsx
+++ app/assets/javascripts/components/appointments.jsx
@@ -1,9 +1,36 @@
 var Appointments = React.createClass({
+    getInitialState: function() {
+        return {
+            appointments: this.props.appointments,
+            title: 'Team meeting in foyer',
+            appt_time: 'Tomorrow at 9am'
+        }    
+    },
+    handleUserInput: function(obj) {
+        this.setState(obj)    
+    },
+    handleFormSubmit: function() {
+        var appointment = {title: this.state.title, appt_time: this.state.appt_time}
+        $.post('/appointments',
+                {appointment: appointment})
+                .done(function(data) {
+                    this.addNewAppointment(data);
+                });
+    },
+    
+    
+    addNewAppointment: function(appointment) {
+        
+    },
+    
     render: function() {
         return (
             <div>
-                <AppointmentForm />
-                <AppointmentsList appointments={this.props.appointments} />
+                <AppointmentForm input_title={this.state.title} 
+                    input_appt_time={this.state.appt_time}
+                    onUserInput={this.handleUserInput}
+                    onFormSubmit={this.handleFormSubmit} />
+                <AppointmentsList appointments={this.state.appointments} />
             </div>
             )
     }
