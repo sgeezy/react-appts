52f2f532d3c0aa144ebc8575cd368b9cf7c52fd2
sgeezy <gmixlpixl@gmail.com>
2017-02-03 20:12:02 +0000

    Complete lesson 4.4 (last time was 4.3...)

--- app/assets/javascripts/components/appointments_list.es6.jsx
+++ app/assets/javascripts/components/appointments_list.es6.jsx
@@ -1,13 +1,8 @@
-class AppointmentsList extends React.Component {
-    render () {
-        return(
-            <div>
-                {this.props.appointments.map(function(appointment) {
-                    return(
-                        <Appointment appointment={appointment} key = {appointment.id} />
-                    )
-                })}
-            </div>
-        )
-    }
-}
+const AppointmentsList = ({appointments}) =>
+    <div>
+        {appointments.map(function(appointment) {
+            return(
+                <Appointment appointment={appointment} key = {appointment.id} />
+            )
+        })}
+    </div>
